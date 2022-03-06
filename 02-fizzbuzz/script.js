8d1e5ebbb7493b05d1a5dbdb503227ca08c54711
Klyga Dima <k4901270@gmail.com>
2022-03-06 12:31:21 +0300

    master

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -11,20 +11,15 @@
 */
 
 function fizzBuzz(n) {
-    if (n % 5 === 0 && n % 3 === 0) {
-        return 'fizzBuzz';
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 == 0) console.log("fizzbuzz");
+        else if (i % 3 == 0) console.log("fizz");
+        else if (i % 5 == 0) console.log("buzz");
+        else console.log(i);
     }
-    if (n % 3 === 0) {
-        return 'fizz';
-    } 
-    if (n % 5 === 0) {
-        return 'buzz';
-    }
-return n;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
+
 console.log(fizzBuzz(15));
-console.log(fizzBuzz(1));
-console.log(fizzBuzz(10));
